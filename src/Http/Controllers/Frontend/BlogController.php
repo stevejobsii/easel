4e8d5368443ebfb53a6e0b606eb277e93ea07200
di <stevejobsii@outlook.com>
2017-03-30 19:45:09 +0800

    test

--- src/Http/Controllers/Frontend/BlogController.php
+++ src/Http/Controllers/Frontend/BlogController.php
@@ -28,7 +28,8 @@ class BlogController extends Controller
         $css = Settings::customCSS();
         $js = Settings::customJS();
 
-        return view($layout, $data, compact('css', 'js', 'socialHeaderIconsUser'));
+return $layout;
+        //return view($layout, $data, compact('css', 'js', 'socialHeaderIconsUser'));
     }
 
     /**
