b1142121f297b4967f26a5804c6854351bc5c050
di <stevejobsii@outlook.com>
2017-03-31 17:21:08 +0800

    css

--- resources/views/frontend/blog/partials/posts.blade.php
+++ resources/views/frontend/blog/partials/posts.blade.php
@@ -3,24 +3,28 @@
         <h2 class="post-title">
             <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
         </h2>
+      <div style="position: relative;">
+            <div>
+                <p class="post-meta">
+                    {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
+                    <br>
+                    @unless( $post->tags->isEmpty())
+                        {!! implode(' ', $post->tagLinks()) !!}
+                    @endunless
+                </p>  
+                
+                <p class="postSubtitle">
+                    {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
+                </p>
 
-        <div><!-- 左边 -->
-        <p class="post-meta">
-            {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
-            <br>
-            @unless( $post->tags->isEmpty())
-                {!! implode(' ', $post->tagLinks()) !!}
-            @endunless
-        </p>
-        <p class="postSubtitle">
-            {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
-        </p>
-        <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
-        </div>
+                <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+            </div>
 
-        <div style="  position:absolute;right:5px;"><!-- 右边 -->
-        <a href="{{ $post->url($tag) }}"><img src="{{$post->page_image}}" alt="{{ $post->title }}"></a>
-        </div>
+            <div style="position:relative; float:right;margin-left: 20px;">
+                <a href="{{ $post->url($tag) }}"><img style=" height: 200px;width: auto;" src="{{$post->page_image}}" alt="{{ $post->title }}"></a>
+           </div>
+        
+      </div>
 
     </div>
     <hr>
