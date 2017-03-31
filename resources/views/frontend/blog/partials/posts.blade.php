b9dd3bd65dc8d7c3dbff3a26bccd4a32a4484f84
di <stevejobsii@outlook.com>
2017-03-31 16:59:33 +0800

    test css

--- resources/views/frontend/blog/partials/posts.blade.php
+++ resources/views/frontend/blog/partials/posts.blade.php
@@ -3,6 +3,8 @@
         <h2 class="post-title">
             <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
         </h2>
+
+        <div><!-- 左边 -->
         <p class="post-meta">
             {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
             <br>
@@ -14,6 +16,12 @@
             {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
         </p>
         <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+        </div>
+
+        <div style="  position:absolute;right:5px;"><!-- 右边 -->
+        <a href="{{ $post->url($tag) }}"><img src="{{$post->page_image}}" alt="{{ $post->title }}"></a>
+        </div>
+
     </div>
     <hr>
 @endforeach
